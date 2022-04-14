327de831d44ab3f8834f743ed7128331a40b454d
Kaisar Yerdenbekov <sysreqx@gmail.com>
2022-04-14 21:48:55 +0600

    SessionFactory. Session.

---
+++ src/main/java/hibernate_test_package/Test1.java
@@ -0,0 +1,27 @@
+package hibernate_test_package;
+
+import hibernate_test_package.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            Employee employee = new Employee("Kaisar", "Yerdenbekov", "IT", 10000);
+            session.beginTransaction();
+            session.save(employee);
+            session.getTransaction().commit();
+        }
+        finally {
+            sessionFactory.close();
+        }
+    }
+}
